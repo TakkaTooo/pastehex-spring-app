7c8ff72d89f4cfb548d30914ad5b6953c278585e
Maxim Astashkin <maksim.astash@gmail.com>
2021-10-07 21:00:13 +0300

    Initial commit

---
+++ src/main/java/ru/rsreu/pastehex/PastehexApplication.java
@@ -0,0 +1,13 @@
+package ru.rsreu.pastehex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastehexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastehexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rsreu/pastehex/PastehexApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsreu.pastehex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastehexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
